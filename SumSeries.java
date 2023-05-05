20bb12c1b92f6a28d1d60b0d452bc206f6745966
Mario Sarihaddu <mario.jeswanth@gmail.com>
2023-05-05 12:32:54 +0530

    first commit

---
+++ SumSeries.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumSeries{
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		double sum=0.0;
+		for(double i=1;i<=n;i++)
+		{
+			sum = sum+1/i;
+		}
+		System.out.println("Sum:" +sum);
+
+
+
+	}
+}
+ 
